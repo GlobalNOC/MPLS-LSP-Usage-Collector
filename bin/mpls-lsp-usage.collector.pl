de847736304cac672d19811662955990e78fd3a2
Charles Kloote <ckloote@iu.edu>
2016-06-29 15:31:37 +0000

    fixup daemon script

--- bin/mpls-lsp-usage.collector.pl
+++ bin/mpls-lsp-usage.collector.pl
@@ -3,46 +3,45 @@
 use strict;
 use warnings;
 
-use GRNOC::MPLS::Collector;
 use Getopt::Long;
 
-### constants ###
+use GRNOC::Log;
+use GRNOC::Config;
+use GRNOC::MPLS::Collector;
 
 use constant DEFAULT_CONFIG_FILE => "/etc/grnoc/mpls-lsp-usage-collector/config.xml";
-use constant DEFAULT_LOGGING_FILE => "/etc/grnoc/mpls-lsp-usage-collector/logging.conf";
-use constant DEFAULT_PID_FILE => "/var/run/mpls-lsp-usage-collector.pid";
 
-# command line options
 my $config_file = DEFAULT_CONFIG_FILE;
-my $logging_file = DEFAULT_LOGGING_FILE;
-my $pid_file = DEFAULT_PID_FILE;
-my $nofork;
+my $nofork = 0;
+my $debug = 0;
 my $help;
 
 GetOptions( "config|c=s" => \$config_file,
-            "logging=s" => \$logging_file,
-            "pid-file=s" => \$pid_file,
             "nofork" => \$nofork,
+	    "debug|d" => \$debug,
             "help|h|?" => \$help,
-
-# did they ask for help?
-usage() if ( $help );
-
-my $grnoc_log = new GRNOC::Log(
-    config => $logging_file
     );
 
-my $daemonize = !$nofork;
+
+usage() if ( $help );
+
+my $config = GRNOC::Config->new(
+    'config_file' => $config_file,
+    'force_array' => 0
+    );
+
+my $grnoc_log = GRNOC::Log->new(
+    'config' => $config->get('/config/logging/@config-file')
+    );
 
 my $collector = GRNOC::MPLS::Collector->new( config_file => $config_file,
-					     pid_file => $pid_file,
-					     daemonize => $daemonize,
+					     daemonize => !$nofork,
     );
 
 $collector->start();
 
 sub usage {
-    print "$0 [--config <config file>] [--logging <logging config file>] [--pid-file <pid file>] [--nofork] [--timerange <seconds>] [--runonce] [--help]\n";
+    print "$0 [--config <config file>] [--debug] [--help]\n";
     print "\t--nofork - Do not daemonize\n";
     exit( 1 );
 }
